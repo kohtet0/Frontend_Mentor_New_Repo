122f3c4bbcaf083d2f07b0eed4058f87d648d3c9
Thwin Lin Htet <thwinlinhtetthwinlinhtet@gmail.com>
2023-11-14 00:55:30 +0630

    uiok

---
+++ 9/article-preview-component-master/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Manrope", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        mobile: "375px",
+        // => @media (min-width: 375px) { ... }
+
+        desktop: "1440px",
+        // => @media (min-width: 1440px) { ... }
+      },
+      colors: {
+        vdgb: "hsl(217, 19%, 35%)",
+        ddb: "hsl(214, 17%, 51%)",
+        gb: "hsl(212, 23%, 69%)",
+        lgb: "hsl(210, 46%, 95%)",
+      },
+      fontSize: {
+        fs: "13px",
+      },
+    },
+  },
+  plugins: [],
+};
